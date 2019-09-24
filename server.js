06c0c19561f5025ac2e46b92bcb3ad2de9cf5223
Steven Bull <stevenwbulljr@gmail.com>
2019-09-24 14:44:47 -0500

    finish api key authorization middleware

--- server.js
+++ server.js
@@ -9,11 +9,25 @@ const authToken = process.env.API_TOKEN;
 
 const app = express();
 
+//add all middleware
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 
-app.get('/movie', (req, res) => {
+function validateBearerToken( req, res, next ) {
+  const userToken = req.get('Authorization');
+  console.log(`Validate bearer token middleware executed. Token key is ${userToken}`);
+  console.log(authToken);
+  if (!userToken || userToken.split(' ')[1] !== authToken) {
+    return res.status(401).json('error: Unauthorized request')
+  }
+  next();
+}
+
+app.use(validateBearerToken);
+
+//endpoint date manipulation
+app.get('/movie', ( req, res ) => {
   res.json('I am running!');
 })
 
