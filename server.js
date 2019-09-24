9f898353c872d30679ed6a5326d318733d05207a
Steven Bull <stevenwbulljr@gmail.com>
2019-09-24 15:44:22 -0500

    implement DRY methodology, clean up code base in server.js

--- server.js
+++ server.js
@@ -15,11 +15,14 @@ app.use(cors());
 
 //add token authorization
 function validateBearerToken( req, res, next ) {
+
   const authToken = process.env.API_TOKEN;
   const userToken = req.get('Authorization');
+
   if (!userToken || userToken.split(' ')[1] !== authToken) {
     return res.status(401).json('error: Unauthorized request')
   }
+
   next();
 }
 
@@ -27,18 +30,20 @@ app.use(validateBearerToken);
 
 //endpoint data manipulation
 app.get('/movie', ( req, res ) => {
+  
+  const { genre, country, avg_vote } = req.query;
   let results = movies;
 
-  if (req.query.genre) {
-    results = results.filter( movie => movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()));
+  if (genre) {
+    results = results.filter( movie => movie.genre.toLowerCase().includes(genre.toLowerCase()));
   }
 
-  if (req.query.country) {
-    results = results.filter( movie => movie.country.toLowerCase().includes(req.query.country.toLowerCase()));
+  if (country) {
+    results = results.filter( movie => movie.country.toLowerCase().includes(country.toLowerCase()));
   }
 
-  if (req.query.avg_vote) {
-    const voteResults = results.filter( movie => parseFloat(movie.avg_vote) >= parseFloat(req.query.avg_vote));
+  if (avg_vote) {
+    const voteResults = results.filter( movie => parseFloat(movie.avg_vote) >= parseFloat(avg_vote));
     results = voteResults.sort(( a, b ) => a.avg_vote < b.avg_vote ? 1 : a.avg_vote > b.avg_vote ? -1 : 0);
   }
 
