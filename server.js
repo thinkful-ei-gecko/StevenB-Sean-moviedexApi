a59a63c2c693455146ae1de91e616d37326331a0
Steven Bull <stevenwbulljr@gmail.com>
2019-09-24 15:37:29 -0500

    finish full functionality of moviedex-api server

--- server.js
+++ server.js
@@ -27,7 +27,22 @@ app.use(validateBearerToken);
 
 //endpoint data manipulation
 app.get('/movie', ( req, res ) => {
-  res.json('I am running!');
+  let results = movies;
+
+  if (req.query.genre) {
+    results = results.filter( movie => movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()));
+  }
+
+  if (req.query.country) {
+    results = results.filter( movie => movie.country.toLowerCase().includes(req.query.country.toLowerCase()));
+  }
+
+  if (req.query.avg_vote) {
+    const voteResults = results.filter( movie => parseFloat(movie.avg_vote) >= parseFloat(req.query.avg_vote));
+    results = voteResults.sort(( a, b ) => a.avg_vote < b.avg_vote ? 1 : a.avg_vote > b.avg_vote ? -1 : 0);
+  }
+
+  res.json(results);
 })
 
 const PORT = 8000;
